d28f09339d2f353484eb5a235ff8f2ef45f1aee3
SAURABH KUMAR <87616133+Saurabhk1975@users.noreply.github.com>
2025-11-23 14:29:42 +0530

    Delete Farmage directory

--- Farmage/src/components/ComingSoonBadge.jsx
+++
@@ -1,7 +0,0 @@
-export default function ComingSoonBadge() {
-  return (
-    <span className="inline-block bg-red-600 text-white text-xs font-bold px-3 py-1 rounded-full ml-2">
-      COMING SOON
-    </span>
-  );
-}
