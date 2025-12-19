9bf3a0beacd2de7b90cd1831d0798668d2ec34dc
SAURABH KUMAR <87616133+Saurabhk1975@users.noreply.github.com>
2025-12-19 12:52:08 +0530

    Update contact information in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer(){
     <footer className="mt-auto bg-white/60 border-t">
       <div className="container mx-auto px-4 md:px-6 py-6 flex flex-col md:flex-row justify-between items-center text-sm text-brown gap-2">
         <div>© {new Date().getFullYear()} Farmage — Madhubani Origin</div>
-        <div>Made with care • Mumbai • <a href="mailto:farmagestore@gmail.com" className="underline">farmagestore@gmail.com</a>• <a className="underline"> +91 - 9662966244</a>
+        <div>Made with care • Mumbai • <a href="mailto:farmagestore@gmail.com" className="underline">farmagestore@gmail.com</a>• <a className="underline">  <strong>+91-9662966244, +91-9769157252</strong></a>
         </div>
       </div>
     </footer>
