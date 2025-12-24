075f30ccbd3d0440ec1649506b269accf8dbc18a
SAURABH KUMAR <87616133+Saurabhk1975@users.noreply.github.com>
2025-12-24 11:32:38 +0530

    Update company description in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,14 +3,14 @@ export default function About(){
     <main className='container mx-auto px-4 md:px-6 py-12'>
       <h2 className='text-2xl font-bold text-darkgreen'>Our Story</h2>
       <p className='mt-4 text-brown'>
-        Farmage is a Mumbai–based premium makhana brand directly connected with the
+        Farmage is a unit of Paavitram Food Pvt Ltd,  a Mumbai-based premium makhana brand directly connected with the
         farmers of Madhubani, Bihar — the birthplace of the world’s finest fox nuts.
         We manage our entire supply chain end-to-end: sourcing, hand-processing,
         cleaning, grading, roasting, and flavouring, all at our own facility.
 
         <br /><br />
         We supply makhana across India and also cater to <strong>bulk orders,
-        wholesale requirements, private labeling, international export shipments and
+        wholesale requirements, private labeling, international export shipments, and
         custom flavour production.</strong> Our processing standards ensure consistent
         quality, proper moisture levels, and export-grade packaging.
 
